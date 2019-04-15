b91a7d6b8ff06a5d5cca1a10ceb2133ba4d2f92a
ParkhomenkoAlexey <Parkhomenko_AD@mail.ru>
2019-04-15 17:25:14 +0300

    Initial Commit

---
+++ PrettyAlert/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  PrettyAlert
+//
+//  Created by Алексей Пархоменко on 08/04/2019.
+//  Copyright © 2019 Алексей Пархоменко. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+        
+    }
+    
+}
+
